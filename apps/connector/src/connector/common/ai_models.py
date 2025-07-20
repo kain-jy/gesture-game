8433df822e9d2856bf183dbc20a864ed732a09e4
motty <motty.mio2@gmail.com>
2025-07-20 11:24:10 +0900

    model changes

--- apps/connector/src/connector/common/ai_models.py
+++ apps/connector/src/connector/common/ai_models.py
@@ -1,8 +1,8 @@
 ai_models: dict[str, str] = {
-    "amazon": "amazon.nova-pro-v1:0",
+    "amazon": "us.amazon.nova-lite-v1:0",
     "anthropic": "us.anthropic.claude-3-7-sonnet-20250219-v1:0",
     # "meta": "us.meta.llama4-maverick-17b-instruct-v1:0",
     # "stability": "us.stability.sd3-5-large-v1:0",
-    "marverick": "us.meta.llama4-maverick-17b-instruct-v1:0",
+    "maverick": "us.meta.llama4-maverick-17b-instruct-v1:0",
     "scout": "us.meta.llama4-scout-17b-instruct-v1:0",
 }
